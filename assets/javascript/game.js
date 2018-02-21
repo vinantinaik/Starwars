bcefe3b0ad227adb99580de22590caed80be48b7
Vinanti <vinantinaik@gmail.com>
2018-02-20 22:00:59 -0600

    added game part 1 choosing player, defender and attack

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,194 @@
 
 
-var starWarCharacters=[{name:"han",
-                        healthPoints:120,
-                        attackPoints:8,
-                        counterAttackPoints:10,
-                        img:"./assets/images/1.png"},
-                        {name:"rey",
-                        healthPoints:180,
-                        attackPoints:5,
-                        counterAttackPoints:15,
-                        img:"./assets/images/2.png"},
-                        {name:"mace",
-                        healthPoints:150,
-                        attackPoints:9,
-                        counterAttackPoints:15,
-                        img:"./assets/images/3.png"},
-                        {name:"leia",
-                        healthPoints:200,
-                        attackPoints:5,
-                        counterAttackPoints:6,
-                        img:"./assets/images/4.png"}]
+var starWarCharacters = [{
+    name: "han",
+    healthPoints: 120,
+    attackPoints: 8,
+    counterAttackPoints: 10,
+    img: "./assets/images/1.png"
+},
+{
+    name: "rey",
+    healthPoints: 180,
+    attackPoints: 5,
+    counterAttackPoints: 15,
+    img: "./assets/images/2.png"
+},
+{
+    name: "mace",
+    healthPoints: 150,
+    attackPoints: 9,
+    counterAttackPoints: 15,
+    img: "./assets/images/3.png"
+},
+{
+    name: "leia",
+    healthPoints: 200,
+    attackPoints: 5,
+    counterAttackPoints: 6,
+    img: "./assets/images/4.png"
+}]
+
+var game = {
+    player: undefined,
+    defender: undefined,
+    enemies: undefined,
+    won: false,
+    lost: false,
+
+
+    attack: function () {
+        this.defender[0].healthPoints -= this.player[0].attackPoints;
+        this.player[0].healthPoints -= this.defender[0].counterAttackPoints;
+        if (this.player[0].healthPoints <= 0 && this.defender[0].healthPoints > 0) {
+            this.lost = true;
+        }
+        else if (this.defender[0].healthPoints <= 0) {
+            this.won = true;
+           
+        }
+        else
+        {
+            this.power();
+        }
+
+    },
+
+    power: function () {
+        this.player[0].attackPoints += this.player[0].attackPoints;
+    }
 
 
 
-var player=[];
-var defender=[];
-var enemies=[];
+
+
+
+}
+
+
+
 
 $(document).ready(function () {
+    $(".btn-danger").hide();
+    addCharacters(starWarCharacters, "#allCharacters");
 
-    addCharacters(starWarCharacters,"#allCharacters");
-    
-    function addCharacters(list,holder)
-    {
+    function addCharacters(list, holder) {
         $(holder).empty();
-        $.each(list,function(idx,obj){
-         var imgSrc = obj.img;
-         var imgTag = $("<img>");
-         imgTag.attr("src",imgSrc);
-         imgTag.attr("alt",obj.name);
+        $.each(list, function (idx, obj) {
 
-         var btnTag = $("<button>");
-         btnTag.addClass("btn");
-         btnTag.attr("value",obj.name);
-         var div_col = $("<div>");
-        
-         div_col.addClass("col-lg-3");
-         btnTag.append(imgTag);
-         div_col.append(btnTag);
+            var div_col = $("<div>");
+            div_col.addClass("col-lg-3");
 
-         $(holder).append(div_col);
+            var h3Tag = $("<h3>");
+            
+            h3Tag.addClass("text-center");
+            h3Tag.html(obj.healthPoints);
+
+            var divCard = $("<div>");
+            divCard.addClass("card");
+
+            var divCardHeader = $("<div>");
+            divCardHeader.addClass("card-header text-center");
+            divCardHeader.html(obj.name);
+
+            var divCardBody = $("<div>");
+            divCardBody.addClass("card-body");
+
+            var imgSrc = obj.img;
+            var imgTag = $("<img>");
+            imgTag.addClass("card-img-top");
+            imgTag.attr("src", imgSrc);
+            imgTag.attr("alt", obj.name);
+
+            var btnTag = $("<button>");
+            btnTag.addClass("btn");
+            btnTag.attr("value", obj.name);
+
+
+
+            $(btnTag).append(imgTag);
+
+            $(divCardBody).append(btnTag);
+            $(divCardBody).append(h3Tag);
+
+            $(divCard).append(divCardHeader);
+            $(divCard).append(divCardBody);
+            $(div_col).append(divCard);
+            $(holder).append(div_col);
 
 
         });
     }
 
-    $("body").on("click","#allCharacters .btn",function(){
+    $("body").on("click", "#allCharacters .btn", function () {
 
-        var character =$(this).val();
-        
-        player= $.grep(starWarCharacters,function(obj,i){
+        var character = $(this).val();
 
-            return(obj.name === character);
+        game.player = $.grep(starWarCharacters, function (obj, i) {
+
+            return (obj.name === character);
         });
 
-        enemies=$.grep(starWarCharacters,function(obj,i){
+
+
+
+        game.enemies = $.grep(starWarCharacters, function (obj, i) {
 
             return (obj.name !== character);
         });
 
-        addCharacters(player,"#yourCharacter");
+        addCharacters(game.player, "#yourCharacter");
 
-        addCharacters(enemies,"#enemies");
+        addCharacters(game.enemies, "#enemies");
 
         $("#allCharacters").empty();
-    })
+    });
 
-    $("body").on("click","#enemies .btn",function(){
+    $("body").on("click", "#enemies .btn", function () {
 
-        var character =$(this).val();
-        
-        defender= $.grep(starWarCharacters,function(obj,i){
+        var character = $(this).val();
 
-            return(obj.name === character);
+        game.defender = $.grep(starWarCharacters, function (obj, i) {
+
+            return (obj.name === character);
         });
 
-        enemies=$.grep(starWarCharacters,function(obj,i){
 
-            return (obj.name !== character && obj.name !==player[0].name);
+
+        game.enemies = $.grep(starWarCharacters, function (obj, i) {
+
+            return (obj.name !== character && obj.name !== game.player[0].name);
         });
 
-        addCharacters(defender,"#defender");
+        addCharacters(game.defender, "#defender");
 
-        addCharacters(enemies,"#enemies");
+        addCharacters(game.enemies, "#enemies");
+        $(".btn-danger").show();
 
-       // $("#allCharacters").empty();
-    })
-    
+    });
+
+    // attack
+    $(".btn-danger").on("click", function () {
+
+
+        game.attack();
+        displayResult();
+
+
+    });
+
+    function updateGame()
+    {
+        if(game.won)
+        {
+            $("#defender").empty();
+        }
+    }
+
+    function displayResult(){
+
+        $("#result").html("you attacked " + game.defender[0].name + " for " + game.player[0].attackPoints + " damage. "
+    + game.defender[0].name + " attacked you for " + game.defender[0].counterAttackPoints);
+    }
+
 });
